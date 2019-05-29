f6a4c75c9802da665fd92035016f16aaf2ebb452
Arslan-SE <39010565+Arslan-SE@users.noreply.github.com>
2019-05-29 05:24:47 -0400

    Update main.tf

--- provision/main.tf
+++ provision/main.tf
@@ -1,38 +1,8 @@
-resource "azurerm_resource_group" "arm" {
-  name     = "${var.azurerm_resource_group_name}"
-  location = "${var.azurerm_resource_group_location}"
-}
+//--------------------------------------------------------------------
+// Modules
+module "3tier" {
+  source  = "app.terraform.io/arslanhashi/3tier/azure"
+  version = "1.0.1"
 
-resource "azurerm_network_security_group" "arm" {
-  name                = "SecurityGroup"
-  location            = "${azurerm_resource_group.arm.location}"
-  resource_group_name = "${azurerm_resource_group.arm.name}"
-}
-
-resource "azurerm_virtual_network" "arm" {
-  name                = "VirtualNetwork"
-  resource_group_name = "${azurerm_resource_group.arm.name}"
-  address_space       = ["10.0.0.0/16"]
-  location            = "${azurerm_resource_group.arm.location}"
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
-
-  subnet {
-    name           = "10.0.1.0_24"
-    address_prefix = "10.0.1.0/24"
-  }
-
-  subnet {
-    name           = "10.0.2.0_24"
-    address_prefix = "10.0.2.0/24"
-  }
-
-  subnet {
-    name           = "10.0.3.0_24"
-    address_prefix = "10.0.3.0/24"
-    security_group = "${azurerm_network_security_group.arm.id}"
-  }
-
-  tags {
-    Purpose = "PoC"
-  }
+  prefix = "AZLMODULE"
 }
